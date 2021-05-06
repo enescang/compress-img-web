fee8c547bdd1a699157c81ef0cb8135cf4b54a8c
kodcanlisi <developerindiee@gmail.com>
2021-05-06 21:04:03 +0300

    Add about page

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+const About = ()=>{
+    return(
+        <div>
+            <h1>Friday Team Ekibi Bilgi Sayfası</h1>
+            <p>Yotube video linleri ve bilgiler buraya en kısa zamanda eklenecek</p>
+        </div>
+    )
+}
+
+export default About;
