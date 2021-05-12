083538535b4240578cff0f3d4a41f4a36c071a46
kodcanlisi <developerindiee@gmail.com>
2021-05-12 20:25:49 +0300

    Helping someone

---
+++ pages/batuhan/index.js
@@ -0,0 +1,15 @@
+const Batu =()=>{
+    return(
+        <div>
+            <p className="">
+            Hocam kısa süre önce internet neredeyse hiç çekmeyen yere geldim ve aynı noktada kalıyorum hala.Bugün saat 16.30 civarında  video yükleme olayından haberim oldu.Başka birinden bilgisayar bulup anca programları yükleyip size gönderebildim.
+            </p>
+            <br/>
+            <a className="text-blue-800" href="https://youtu.be/Xlsmm4stMdI">
+                Sunum Videosu
+            </a>
+        </div>
+    )
+}
+
+export default Batu;
